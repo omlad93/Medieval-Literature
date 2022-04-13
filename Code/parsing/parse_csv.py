41495879cc39062b53a437caeac5fda5f69ca410
Omri Elad <omlad93@gmail.com>
2022-04-13 09:58:14 +0300

    multiple options for parsing CSV: slim/origin, save/not-save, verbose/quiet

--- Code/parsing/parse_csv.py
+++ Code/parsing/parse_csv.py
@@ -3,7 +3,6 @@ import sys
 from pathlib import Path
 from typing import Sequence
 sys.path.insert(0,str(Path(__file__).parent.parent))
-import configargparse as parser
 import os
 import pandas as pd
 from pandas import DataFrame
@@ -23,7 +22,6 @@ def convert_labels(topic_list: list[str])->list[Label]:
             missing_labels.add(topic)
     return topic_list
    
-
 def combine_topics(x) -> list[str]:
     '''
     A function to combine all Topics columns into a single column
@@ -32,33 +30,47 @@ def combine_topics(x) -> list[str]:
     topics = [c for c in x.index if 'Topic' in c]
     return convert_labels([x[topic] for topic in topics if x[topic]])
         
-
-def parse_single_csv(path:str,slim:bool=True, save:bool=False)->DataFrame:
-    print(f'Parsing {path}')
+def parse_single_csv(path:str,slim:bool=True, save:bool=False,verbose:bool=False)->DataFrame:
+    if verbose:
+        print(f'Parsing {path}')
     df = pd.read_csv(path,encoding='unicode_escape', keep_default_na=False)
     if slim:
+        df.rename(columns={'Line':'Fragment'},inplace=True)
         df['Topics'] = df.apply(combine_topics,axis=1)
         df = df[[c for c in df.columns if c in {'Topics','Fragment'}]]  
     if save:
         with open(path.replace('csv','pickle'), 'wb') as pkl:
             pickle.dump(df,pkl)
-            print(f'\t Saved DataFrame as {pkl.name}')
+            if verbose:
+                print(f'\t Saved DataFrame as {pkl.name}')
     return df
 
-def parse_all_csv_in_directory(dir_path:str,slim=True,save:bool=False)->Sequence(DataFrame):
+def parse_all_csv_in_directory(dir_path:str,slim=True,save:bool=False,verbose:bool=False)->Sequence[tuple[DataFrame,str]]:
     return tuple(
-        parse_single_csv(f'{dir_path}{os.sep}{file}',slim,save) 
+        (parse_single_csv(f'{dir_path}{os.sep}{file}',slim,save,verbose),file.split('.')[0])
         for file in os.listdir(dir_path) if file.endswith('csv')
         )
 
+def unpickle_dir(dir_path:str)->Sequence[tuple[DataFrame,str]]:
+    return tuple(
+        (pickle.load(open(f'{dir_path}{os.sep}{file}','rb')),file.split('.')[0])
+        for file in os.listdir(dir_path) if file.endswith('pickle')
+    )
 
+def is_for_training(df:DataFrame)->bool:
+    return any(df['Topics'])
+
+def is_for_filling(df:DataFrame)->bool:
+    return not any(df['Topics'])
 
 
 def main():
     # parse_single_csv("Data//csv//Macbeth.csv")
     # parse_all_csv_in_directory("Data\csv",save=True)
-    parse_all_csv_in_directory("Data\csv")
-    print('Done')
+    parse_all_csv_in_directory("Data\csv", save=True)
+    print()
+    for i,(df,play) in enumerate(unpickle_dir("Data\pickle"),start=1):
+        print(f'{i}) {play:<37} is for {"Training" if is_for_training(df) else "Filling"}')
 
 if __name__ == "__main__":
     main()
